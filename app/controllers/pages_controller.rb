e0ce1de065b04f38704607435023553a2c67559d
Till Richter <robintill81@gmail.com>
2023-08-26 11:12:45 +0200

    logo eingefuegt

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+  def home
+    @members =["Till", "Tomasz", "Andre", "Sascha", "Gerdt", "Gordon Gekko", "Bolle", "Jacky Chan"]
+
+  end
+
+  def about
+  end
+
+  def werwirsind
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: "pages#home"
+  get "about",           to: "pages#about", as: :about
+  get "werwirsind",      to: "pages#werwirsind", as: :werwirsind
+  get "new",             to: "pages#new", as: :new
+
 end
